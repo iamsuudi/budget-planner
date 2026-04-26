6bab6ddd8f118dde9f28179cfa69b7cfaf8cddde
Abdulfetah <suudiabdulfetah@gmail.com>
2026-04-26 10:55:20 +0300

    Setup comprehensive metadata

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,20 @@ import ReactDOM from 'react-dom/client'
 import { RouterProvider, createRouter } from '@tanstack/react-router'
 import { routeTree } from './routeTree.gen'
 
+// Manual Registration
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register('/sw.js') // Points to public/sw.js
+      .then((registration) => {
+        console.log('SW registered: ', registration)
+      })
+      .catch((registrationError) => {
+        console.error('SW registration failed: ', registrationError)
+      })
+  })
+}
+
 const router = createRouter({
   routeTree,
   defaultPreload: 'intent',
